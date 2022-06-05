c5dea984e40bbae19ecc743db60d0016edce589d
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2022-06-05 17:44:24 +0530

    Update streamlit_app.py
    del

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,3 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
-streamlit.text('Idly')
-streamlit.text('Dosa')
-streamlit.text('Idiyappam')
